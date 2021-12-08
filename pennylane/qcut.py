86bcdd7d12473386de752ae4a88196e3cdd4a954
trbromley <brotho02@gmail.com>
2021-12-08 16:20:03 -0500

    Tidy

--- pennylane/qcut.py
+++ pennylane/qcut.py
@@ -1,22 +1,23 @@
 # Copyright 2021 Xanadu Quantum Technologies Inc.
-
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Tuple, Any, Dict, Union, Callable, Optional, Sequence
 from functools import partial
+from typing import Any, Callable, Dict, Optional, Sequence, Tuple, Union
 
-from pennylane.operation import Operation, Operator, AnyWires
-from pennylane.tape import QuantumTape
 from networkx import DiGraph
+
+from pennylane.operation import AnyWires, Operation, Operator
+from pennylane.tape import QuantumTape
 from pennylane.transforms import batch_transform
 
 
@@ -41,7 +42,9 @@ class PrepareNode(Operation):
 
 
 @batch_transform
-def cut_circuit(tape: QuantumTape, method: Optional[Union[str, Callable]] = None, **kwargs) -> Tuple[Tuple[QuantumTape], Callable]:
+def cut_circuit(
+    tape: QuantumTape, method: Optional[Union[str, Callable]] = None, **kwargs
+) -> Tuple[Tuple[QuantumTape], Callable]:
     """Main transform"""
     g = tape.graph.graph.copy()
 
@@ -76,7 +79,13 @@ def find_and_place_cuts(graph: DiGraph, method: Union[str, Callable], **kwargs):
     ...  # calls ``method`` (see ``example_method``) and ``place_cuts``
 
 
-def example_method(graph: DiGraph, max_wires: Optional[int], max_gates: Optional[int], num_partitions: Optional[int], **kwargs) -> Tuple[Tuple[Tuple[Operator, Operator, Any]], Dict[str, Any]]:
+def example_method(
+    graph: DiGraph,
+    max_wires: Optional[int],
+    max_gates: Optional[int],
+    num_partitions: Optional[int],
+    **kwargs
+) -> Tuple[Tuple[Tuple[Operator, Operator, Any]], Dict[str, Any]]:
     """Example method passed to ``find_cuts``. Returns a tuple of wire cuts of the form
     ``Tuple[Tuple[Operator, Operator, Any]]`` specifying the wire to cut between two operators. An
     additional results dictionary is also returned that can contain optional optimization results.
